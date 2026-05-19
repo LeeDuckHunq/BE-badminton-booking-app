ac883497e532322e673ba478785cd957b3883d58
LeeDuckHunq <kirokori123@gmail.com>
2026-05-19 14:17:18 +0700

    get user info api

--- src/main/java/com/example/alopoapi/controller/UserController.java
+++ src/main/java/com/example/alopoapi/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.alopoapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class UserController {
 
         return ResponseEntity.ok(userService.getSoLuongUser());
 
+    }
+
+    @GetMapping("/get-user-info/{username}")
+    public ResponseEntity<?> getUserInfo(@PathVariable String username) {
+        return ResponseEntity.ok(userService.userInfo(username));
     }
 }
--- src/main/java/com/example/alopoapi/service/UserService.java
+++ src/main/java/com/example/alopoapi/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -52,5 +53,11 @@ public class UserService {
 
         return (int)userRepo.count();
 
+    }
+    
+    public Optional<UserEntity> userInfo(String username) {
+        
+        return userRepo.findById(username);
+        
     }
 }
