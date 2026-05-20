9d86912ba830c9eac68e4b3512dce3e446720033
LeeDuckHunq <kirokori123@gmail.com>
2026-05-20 16:59:10 +0700

    fix ThongKeService

--- src/main/java/com/example/alopoapi/service/ThongKeService.java
+++ src/main/java/com/example/alopoapi/service/ThongKeService.java
@@ -4,10 +4,12 @@ import com.example.alopoapi.dto.TopCumSanDTO;
 import com.example.alopoapi.repository.PhieuDatSanRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
 import java.awt.print.Pageable;
 import java.util.List;
 
+@Service
 public class ThongKeService {
     @Autowired
     private PhieuDatSanRepo phieuDatSanRepository;
