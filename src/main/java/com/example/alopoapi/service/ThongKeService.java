19f221fac41c53cd4a9621214dc099d338c38dc6
LeeDuckHunq <kirokori123@gmail.com>
2026-05-20 17:17:09 +0700

    fix pageable cast

--- src/main/java/com/example/alopoapi/service/ThongKeService.java
+++ src/main/java/com/example/alopoapi/service/ThongKeService.java
@@ -4,19 +4,19 @@ import com.example.alopoapi.dto.TopCumSanDTO;
 import com.example.alopoapi.repository.PhieuDatSanRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Service
 public class ThongKeService {
+
     @Autowired
     private PhieuDatSanRepo phieuDatSanRepository;
 
     public List<TopCumSanDTO> getTop10CumSanDatNhieuNhat() {
-        Pageable top10 = (Pageable) PageRequest.of(0, 10);
-
-        return phieuDatSanRepository.findTopCumSan(top10);
+        Pageable pageable = PageRequest.of(0, 10);
+        return phieuDatSanRepository.findTopCumSan(pageable);
     }
 }
